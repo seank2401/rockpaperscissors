0063f94800e1c59dc2a426a8fb20a1dfb0fa088a
Sean <seankerr2401@gmail.com>
2022-08-20 14:19:33 -0500

    add buttons and scoreboard to page, open modal when game finishes

--- javascript.js
+++ javascript.js
@@ -1,93 +1,85 @@
-
-//global variables
+//global variables for computer options and rounds won
+const options = ["rock", "paper", "scissors"];
 let playerWins = 0;
 let computerWins = 0;
-const options = ["rock", "paper", "scissors"];
-var rounds = 0;
+
+//selectors
+const modal = document.getElementById("modalContainer");
+const overlay = document.getElementById("overlay");
+const displayRoundResult = document.querySelector("#roundResult");
+const button = document.querySelectorAll(".playbtn");
+const closeModalButton = document.querySelector("[data-close-button]");
+const updatePlayerWins = document.querySelector("#playerScore");
+const updateComputerWins = document.querySelector("#computerScore");
+const modalUpdate = document.querySelector(".modal");
+
+//modal listeners
+closeModalButton.addEventListener("click", closeModal);
+closeModalButton.addEventListener("click", resetGame);
+
+//iterates through array-like list of buttons and calls playround function on button click
+button.forEach((e) =>
+  e.addEventListener("click", (event) =>
+    playRound(event.currentTarget.id, getComputerChoice())
+  )
+);
+
+//FUNCTIONS
+
+//takes in clicked div id and random computer input
+//displays round result and increments round win totals
+function playRound(userChoice, computerSelection) {
+  if (userChoice == computerSelection) {
+    displayRoundResult.innerHTML = "Tie!";
+  } else if (
+    (userChoice == "rock" && computerSelection == "scissors") ||
+    (userChoice == "scissors" && computerSelection == "paper") ||
+    (userChoice == "paper" && computerSelection == "rock")
+  ) {
+    playerWins++;
+    updatePlayerWins.innerHTML = "Player Score: " + playerWins;
+    displayRoundResult.innerHTML = "Round Won!";
+  } else {
+    computerWins++;
+    updateComputerWins.innerHTML = "Computer Score: " + computerWins;
+    displayRoundResult.innerHTML = "Round Lost!";
+  }
+  checkGame();
+}
+
+function checkGame() {
+  if (playerWins == 5 || computerWins == 5) {
+    displayRoundResult.innerHTML = "Game Over";
+    displayWinnerInModal();
+    openModal();
+  }
+}
+
+function displayWinnerInModal() {
+  playerWins > computerWins
+    ? (modalUpdate.innerHTML = "You Won!")
+    : (modalUpdate.innerHTML = "You Lost!");
+}
+
+function openModal() {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+function resetGame() {
+  playerWins = 0;
+  computerWins = 0;
+  displayRoundResult.innerHTML = "Game Reset";
+  updateComputerWins.innerHTML = "Computer Score: ";
+  updatePlayerWins.innerHTML = "Player Score: ";
+}
 
 //randomly returns element of options array
 function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
 }
-
-//takes in user input, also handles null and incorrect values
-function getUserChoice() {
-  let input = prompt("Rock, Paper, or Scissors?");
-  while (input == null) {
-    input = prompt("Please enter Rock, Paper, or Scissors");
-  }
-  input = input.toLowerCase();
-  let check = validateInput(input);
-  while (check == false) {
-    input = prompt(
-      "Type rock, paper, or scissors. spelling is important but capitalization is not important."
-    );
-    while (input == null) {
-      input = prompt("Please enter Rock, Paper, or Scissors2");
-    }
-    input = input.toLowerCase();
-    check = validateInput(input);
-  }
-  return input;
-}
-
-//validates user input, taking in input and returning true if options contains input, false if not
-function validateInput(choice) {
-  return options.includes(choice);
-}
-
-//plays one round, returning tie, win, or loss based on evaluation
-function playRound() {
-  const computerSelection = getComputerChoice()
-  const userChoice = getUserChoice();
-  if (userChoice == computerSelection) {
-    return "tie"
-  }
-  else if (
-    (userChoice == "rock" && computerSelection == "scissors") ||
-    (userChoice == "scissors" && computerSelection == "paper") ||
-    (userChoice =="paper" && computerSelection=="rock")
-  ) {
-    return "win";
-  }
-  else {
-    return "lost";
-  }
-}
-
-//runs 5 instances of playRound
-function game() {
-  var keepGoing = true;
-  var result;
-  while (keepGoing) {
-    console.log("round: " + (rounds + 1));
-    result = playRound();
-    console.log(result);
-
-    if (result == "win") {
-      rounds++;
-      playerWins++;
-    } else if (result == "lost") {
-      rounds++;
-      computerWins++;
-    } else if (result == "tie") {
-      rounds++;
-    }
-    if (rounds == 5) {
-      keepGoing = false;
-    }
-    console.log("Player rounds won: " + playerWins);
-    console.log("Computer rounds won: " + computerWins);
-  }
-  if (playerWins > computerWins) {
-    console.log("You won the game");
-  } else if (computerWins > playerWins) {
-    console.log("better luck next time");
-  } else {
-    console.log("Tie Game");
-  }
-  console.log("Player rounds won: " + playerWins);
-  console.log("Computer rounds won: " + computerWins);
-}
-
-game();
